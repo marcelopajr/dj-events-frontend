404837135058419e19dc5b7d56b56297bd4c8b8f
Marcelo Pereira <marcelo96jr@gmail.com>
2021-08-29 16:08:34 -0300

    getStaticProps with getStaticPaths

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,9 +1,45 @@
+import { API_URL } from '@/config/index';
 import Layout from '@/components/Layout';
 
-export default function EventPage() {
+export default function EventPage({ event }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{event.name}</h1>
     </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((event) => ({ params: { slug: event.slug } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      event: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       event: events[0],
+//     },
+//   };
+// }
