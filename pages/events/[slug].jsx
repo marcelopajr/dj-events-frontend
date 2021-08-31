db21d571479328404db4e00df5d8c2572348bf78
Marcelo Pereira <marcelo96jr@gmail.com>
2021-08-30 22:08:07 -0300

    Deleting events

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,13 +1,30 @@
 import { API_URL } from '@/config/index';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 import Layout from '@/components/Layout';
 import styles from '@/styles/Event.module.css';
 
 export default function EventPage({ event }) {
-  const deleteEvent = (e) => {
-    console.log('delete');
+  const router = useRouter();
+
+  const deleteEvent = async (e) => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${event.id}`, {
+        method: 'DELETE',
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.push('/events');
+      }
+    }
   };
 
   return (
@@ -28,6 +45,8 @@ export default function EventPage({ event }) {
           {new Date(event.date).toLocaleDateString('en-US')} at {event.time}
         </span>
         <h1>{event.name}</h1>
+
+        <ToastContainer />
 
         {event.image && (
           <div className={styles.image}>
