5eb49855662d565d1e14e80705a319d9656014e1
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-03 20:53:07 -0300

    Get token and persist user

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
   // Register user
   const register = async (user) => {
     console.log(user);
@@ -24,10 +28,10 @@ export const AuthProvider = ({ children }) => {
     });
 
     const data = await res.json();
-    console.log(data);
 
     if (res.ok) {
       setUser(data.user);
+      router.push('/account/dashboard');
     } else {
       setError(data.message);
       setError(null);
@@ -41,7 +45,14 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log('Check');
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
