51c34e7a66a6d79426f7bed14f8475605a2ec4b3
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-02 21:10:12 -0300

    LoginPage and RegisterPage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Search from './Search';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
@@ -23,6 +24,13 @@ export default function Header() {
           <li>
             <Link href="/events/add">
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-secondary btn-icon">
+                <FaSignOutAlt /> Login
+              </a>
             </Link>
           </li>
         </ul>
